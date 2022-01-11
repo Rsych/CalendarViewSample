09b52a79c0d319cc2e351ce1296ee2480f6987c2
Ryan <tamielryan@gmail.com>
2022-01-11 14:01:32 +0200

    Initial Commit

---
+++ CalendarViewSample/CalendarViewSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarViewSampleApp.swift
+//  CalendarViewSample
+//
+//  Created by Ryan J. W. Kim on 2022/01/11.
+//
+
+import SwiftUI
+
+@main
+struct CalendarViewSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalendarViewSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalendarViewSample
+//
+//  Created by Ryan J. W. Kim on 2022/01/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
